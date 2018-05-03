b8a6f0d1d9373ee3e60216269bb47684aadc0c7e
codeWonderland <alice.easter@mymail.champlain.edu>
2018-05-03 13:53:07 -0400

    Added about page

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,10 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res, next) {
+    res.render('about');
+});
+
 router.get('/getSpirits', function(req, res, next) {
     pool.getConnection(function(err, con) {
         if (err) throw err;
